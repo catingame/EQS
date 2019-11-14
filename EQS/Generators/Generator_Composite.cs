3825a20e2bc3b9ba3da5356c6dbe8d20b2053228
keicoon <keicoon15@gmail.com>
2019-11-14 10:20:56 +0900

    Add generator composite

---
+++ EQS/Generators/Generator_Composite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EQS.Classes;
+
+namespace EQS.Generators
+{
+    public class Generator_Composite : QueryGenerator
+    {
+        public List<QueryGenerator> Generators;
+
+        public Generator_Composite(IQueryContext context) : base(context)
+        {
+        }
+
+        internal override List<QueryItem> DoItemGeneration()
+        {
+            var items = new List<QueryItem>();
+            foreach (var generator in Generators)
+            {
+                items.AddRange(generator.DoItemGeneration());
+            }
+            return items;
+        }
+    }
+}
